fc3729857cb040a878ec13891ac53f40cfc07087
Ritik Ranjan <ranjanritik9977@gmail.com>
2025-07-26 17:41:26 +0000

    The left side of the screen is cropping

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full">
       <DashboardSidebar />
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 sm:ml-14">
         <DashboardHeader />
         <main className="flex-1 p-4 md:p-6 lg:p-8 bg-secondary/40">
           {children}
